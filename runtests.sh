ac5b24d12d6324c81cf4682c7410d7aea2167b47
EdmundHusserl <--add>
2020-05-06 22:46:23 -0400

    download dependencies after SCM poll

--- runtests.sh
+++ runtests.sh
@@ -1,5 +1,6 @@
 source $HOME/.bash_profile
 echo "Running tests..."
-pytest ./tests/unittest.py
+python3 -m pip install -r ./requirements.txt
+python3 -m pytest ./tests/unittest.py
 echo "Compiling..."
 python3 -m PyInstaller -F ./exec.py
